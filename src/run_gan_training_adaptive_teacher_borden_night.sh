29615a457cc192d4d207dca732c5eef9247cd14c
Michael <michael.hamer.stanley@gmail.com>
2023-09-11 21:30:33 -0700

    update run sh file

--- src/run_gan_training_adaptive_teacher_borden_night.sh
+++ src/run_gan_training_adaptive_teacher_borden_night.sh
@@ -3,10 +3,10 @@ detector_b_weights can be selected from the weight you trained in STEP1
 """
 task_model_def="/home/michael/ucdavis/CropGAN/adaptive_teacher/configs/faster_rcnn_VGG_borden_night_helios_raw.yaml"
 dataroot="/home/michael/ucdavis/CropGANData/helios_synthetic_datasets/sytheticVis2bordenNight_n=0/"
-checkpoints_dir="/home/michael/ucdavis/CropGANData/output/BordenNight/adaptive_teacher/"
+checkpoints_dir="/home/michael/ucdavis/CropGANData/output/BordenNight/adaptive_teacher_091123/"
 # detector_a: obj detection on the synthetic source domain
 # detector_b: obj detection on the target real domain
-detector_a_weights="/home/michael/ucdavis/CropGAN/adaptive_teacher/output/borden_night_raw_helios_082523/model_0020499.pth"
+detector_a_weights="/home/michael/ucdavis/CropGAN/adaptive_teacher/output/borden_night_helios_raw_n_0_091123/model_0034499.pth"
 # detector_b_weights="/home/michael/ucdavis/CropGANData/output/BordenNight/train1_val1/checkpoints/best_mAp_yolov3_ckpt.pth"
 python -u train_cropgan.py --dataroot $dataroot \
              --num_threads 4\
