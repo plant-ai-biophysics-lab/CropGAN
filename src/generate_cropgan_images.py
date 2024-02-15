3d8fd8644284b59df7d43e0718f2af4c96b2e73a
Michael <michael.hamer.stanley@gmail.com>
2024-02-14 21:37:26 -0800

    scripts for image generation

--- src/generate_cropgan_images.py
+++ src/generate_cropgan_images.py
@@ -21,7 +21,7 @@ from torch.autograd import Variable
 # Import Crop GAN related libs
 gan_dir = os.path.abspath("../src/")
 sys.path.append(gan_dir)
-from options.train_options import TrainOptions
+from options.image_gen_options import ImageGenOptions
 # from options.test_options import TestOptions
 
 from data import create_dataset
@@ -38,20 +38,10 @@ The most important args to be set are:
 --name: # the folder name 
 """
 
-arguments = "--model double_task_cycle_gan\
-             --checkpoints_dir ../data/models/ \
-             --name Synthetic2bordenNight_Yolo_adaptive_branch_102223\
-             --no_flip\
-             --num_threads 0\
-             --gpu_ids 0\
-             --display_id -1\
-             --preprocess resize_and_crop\
-             --load_size 416\
-             --crop_size 416\
-             --batch_size 1 \
-             --task_model_def /home/michael/ucdavis/CropGAN/yolov3/config/yolov3-tiny.cfg" 
 
-opt = TrainOptions().parse_notebook(arguments.split())
+              
+
+opt = ImageGenOptions().parse()
 
 model = create_model(opt)      # create a model given opt.model and other options
 model.setup(opt)               # regular setup: load and print networks; create schedulers
@@ -59,8 +49,8 @@ model.eval()
 print("MODEL SETUP")
 ## 2. Load pretrained model weights
 
-load_suffix = "../data/models/adaptive_teacher/Synthetic2bordenNight_Yolo_adaptive_branch_102223/latest"
-model.load_networks_from_folder(load_suffix)
+# load_suffix = "../data/models/adaptive_teacher/Synthetic2bordenNight_Yolo_adaptive_branch_102223/latest"
+# model.load_networks_from_folder(opt.cropgan_weights)
 
 print("WEIGHTS LOADED")
 ## 3. Load an systhetic (Domain A) image you want to transfer
@@ -73,11 +63,7 @@ def tensor_to_image(tensor):
         tensor = tensor[0]
     return Image.fromarray(tensor)
 
-out_path = "/home/michael/ucdavis/CropGANData/mhs_cropgan_generated/test/"
-in_path = "/home/michael/ucdavis/CropGANData/helios_synthetic_datasets/sytheticVis2bordenNight_n_0/"
-
-synth_images = glob.glob(in_path + "trainA/*.jpg")
-print(f"len(synth_images): {len(synth_images)}")
+synth_images = glob.glob(opt.image_path + "*.jpg")
 with torch.no_grad():
 
     for img_path in synth_images:
@@ -87,5 +73,5 @@ with torch.no_grad():
         fake_img = model.netG_A(raw_img_tensor)
         fake_img_np = fake_img.detach().cpu().squeeze(0).permute([1, 2, 0])*0.5+0.5
         im = tensor_to_image(fake_img_np)
-        im.save(out_path + img_path.split('/')[-1])
+        im.save(opt.out_path + img_path.split('/')[-1])
 
