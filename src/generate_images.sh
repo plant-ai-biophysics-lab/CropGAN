ae57d42a25053f3adf2c6c252b93e6cbc016647f
Michael <michael.hamer.stanley@gmail.com>
2024-02-14 21:37:26 -0800

    scripts for image generation

--- src/generate_images.sh
+++ src/generate_images.sh
@@ -9,13 +9,18 @@ yolo_b_weights="$cropgan_dir/model_checkpoints/yolo_grl/'BASELINE_k=4_01-21-18-5
 task_model_def="$cropgan_dir/CropGAN/yolo_uda/configs/yolov3-tiny.cfg"
 
 python -u generate_cropgan_images.py \
-    --checkpoints_dir ../data/models/ \
-    --name Synthetic2bordenNight_Yolo_adaptive_branch_102223 \
+    --checkpoints_dir /home/michael/ucdavis/CropGAN/data/models/ \
+    --name Sythetic2bordenNight \
+    --epoch "latest" \
     --no_flip \
     --num_threads 0 \
-    --display_id -1 \
     --load_size 416 \
     --crop_size 416 \
     --batch_size 1 \
-    --task_model_def "/home/michael/ucdavis/CropGAN/yolov3/config/yolov3-tiny.cfg"
+    --task_model_def "/home/michael/ucdavis/CropGAN/yolov3/config/yolov3-tiny.cfg" \
+    --image_path "/home/michael/ucdavis/CropGANData/helios_synthetic_datasets/sytheticVis2bordenNight_n_0/trainA/" \
+    --out_path "/home/michael/ucdavis/CropGANData/mhs_cropgan_generated/test/"
     # --use_grl
+    # --cropgan_weights "/home/michael/ucdaivs/CropGAN/data/models/Sythetic2bordenNight/latest" \
+    
+    
---
+++ src/options/image_gen_options.py
@@ -0,0 +1,17 @@
+from .base_options import BaseOptions
+
+
+class ImageGenOptions(BaseOptions):
+    """
+    This class includes training options.
+    It also includes shared options defined in BaseOptions.
+    """
+    def initialize(self, parser):
+        parser = BaseOptions.initialize(self, parser)
+        # parser.add_argument('--cropgan_weights', type=str, help='Path and prefix for CropGAN weights, passed to load_networks_from_folder().')
+        parser.add_argument('--image_path', type=str, help='Where to read synthetic images.')
+        parser.add_argument('--out_path', type=str, help='Where to write modified images.')
+        parser.add_argument('--phase', type=str, default='gen', help='train, val, test, etc')        
+        self.isTrain = False        
+        
+        return parser
