2365e159e56f5f01c80b08c4a5219d813f4e1013
Amogh Joshi <joshi.amoghn@gmail.com>
2024-02-27 20:46:35 -0800

    Streamlined image generation script

--- src/generate_images_farm.sh
+++ src/generate_images_farm.sh
@@ -1,38 +1,39 @@
 #! /bin/bash -l
 #SBATCH --job-name=paibl
-#SBATCH --output=train.out
+#SBATCH --output=generate.out
 #SBATCH --partition=gpum
 #SBATCH --nodes=1 
 #SBATCH --gpus-per-node=1
-#SBATCH --mail-user=michael.hamer.stanley@gmail.com
+#SBATCH --mail-user=amnjoshi@ucdavis.edu
 #SBATCH --time=48:00:00 # Change the time accordingly
 #SBATCH --mail-type=ALL
-#SBATCH --error=train.err
+#SBATCH --error=generate.out
 #SBATCH --cpus-per-task=12
+
 
 # Load module and load environment
 source ~/.bashrc
-conda activate yolo-uda
+conda activate agml
 unset PYTHONPATH
-WORKDIR="/group/jmearlesgrp/scratch/mhstan/CropGAN/src"
+WORKDIR="/group/jmearlesgrp/scratch/amnjoshi/CropGAN/src"
 cd $WORKDIR
 
-
-cropgan_dir="/group/jmearlesgrp/scratch/mhstan"
-data_dir="/group/jmearlesgrp/intermediate_data/mhstan/yolo_grl_data/BordenNight"
+cropgan_dir="/group/jmearlesgrp/scratch/amnjoshi"
+dataroot="/group/jmearlesgrp/intermediate_data/amnjoshi/yolo_grl_data_final/BordenNight/"
 task_model_def="$cropgan_dir/CropGAN/yolo_uda/configs/yolov3-tiny.cfg"
 
 # the epoch is either a number or latest, it will be used to pull cyclegan checkpoint filenames.
 epoch="latest"
 
 # Same as the training images for training yolo-uda: something like BordenNight/source/train/images
-image_path="$data_dir/source/train/images/"
+image_path="$dataroot/source/train/images/"
 
 # Inside of checkpoints_dir, this is the specific directory for the run you want to use
-run_name="BASELINE_k_4"
+run_name="BEST_k=24"
 
 # Where you want the CropGAN generated images to go
-out_path="$data_dir/cropgan_modified_images/$run_name/$epoch/"
+out_path="$cropgan_dir/cropgan_modified_images/$run_name/$epoch/"
+mkdir -p $out_path
 
 # Where your cyclegan checkpoints are stored (note: run_name is a sub-directory of checkpoints_dir)
 checkpoints_dir="$cropgan_dir/model_checkpoints/cyclegan/"
@@ -49,4 +50,5 @@ python -u generate_cropgan_images.py \
     --batch_size 1 \
     --task_model_def $task_model_def \
     --image_path $image_path \
-    --out_path $out_path
+    --out_path $out_path \
+    --log
--- src/options/image_gen_options.py
+++ src/options/image_gen_options.py
@@ -10,7 +10,8 @@ class ImageGenOptions(BaseOptions):
         parser = BaseOptions.initialize(self, parser)
         parser.add_argument('--image_path', type=str, help='Where to read synthetic images.')
         parser.add_argument('--out_path', type=str, help='Where to write modified images.')
-        parser.add_argument('--phase', type=str, default='gen', help='train, val, test, etc')        
+        parser.add_argument('--phase', type=str, default='gen', help='train, val, test, etc')
+        parser.add_argument('--log', action='store_true', default=False, help='Whether to log the output')
         self.isTrain = False        
         
         return parser
