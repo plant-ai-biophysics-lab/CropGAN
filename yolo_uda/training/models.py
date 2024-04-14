0053bdb9fbe8d1b7a14db355ba4f3e93adc39fdc
Michael <michael.hamer.stanley@gmail.com>
2024-04-14 14:24:09 -0700

    use layer 8 instead of 15 for local disc

--- yolo_uda/training/models.py
+++ yolo_uda/training/models.py
@@ -184,7 +184,7 @@ class LocalDiscriminator(nn.Module):
     A 3-layer MLP + Gradient Reversal Layer for domain classification.
     """
 
-    def __init__(self, in_size=255, alpha=1.0, context = False):
+    def __init__(self, in_size=256, alpha=1.0, context = False):
         """
         Arguments:
             in_size: size of the input
@@ -364,7 +364,7 @@ class GRLDarknet(Darknet):
         img_size = x.size(2)
         layer_outputs, yolo_outputs = [], []
         # Use different feature map layers if yolov3 vs. yolov3-tiny
-        feature_map_layers = [15,22] if self.use_tiny else [81,105]
+        feature_map_layers = [8,22] if self.use_tiny else [81,105]
         for i, (module_def, module) in enumerate(zip(self.module_defs, self.module_list)):
             if module_def["type"] in ["convolutional", "upsample", "maxpool"]:
                 x = module(x)
@@ -408,7 +408,7 @@ class GRLDarknet(Darknet):
         img_size = x.size(2)
         layer_outputs, yolo_outputs = [], []
         # Use different feature map layers if yolov3 vs. yolov3-tiny
-        feature_map_layers = [15, 22] if self.use_tiny else [81, 93, 105]
+        feature_map_layers = [8, 22] if self.use_tiny else [81, 93, 105]
         for i, (module_def, module) in enumerate(zip(self.module_defs, self.module_list)):
             if module_def["type"] in ["convolutional", "upsample", "maxpool"]:
                 x = module(x)
@@ -456,7 +456,7 @@ class GRLDarknet(Darknet):
         img_size = x.size(2)
         layer_outputs, yolo_outputs = [], []
         # Use different feature map layers if yolov3 vs. yolov3-tiny
-        feature_map_layers = [15, 22] if self.use_tiny else [81, 93, 105]
+        feature_map_layers = [8, 22] if self.use_tiny else [81, 93, 105]
         for i, (module_def, module) in enumerate(zip(self.module_defs, self.module_list)):
             if module_def["type"] in ["convolutional", "upsample", "maxpool"]:
                 x = module(x)
